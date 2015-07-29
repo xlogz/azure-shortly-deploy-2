6a44168322dbbcb633bfe36a0f95e80ee70b15db
Logan <loganxlow@gmail.com>
2015-07-29 15:42:29 -0700

    Completed options for Grunt dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,146 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: '',
+      },
+      dist: {
+        src: ['public/client/*.js'],
+        dest: 'public/dist/built.js',
+      },
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        } 
+      }
+    },
+
+    jshint: {
+      files: [
+        'public/**/*.js'
+      ],
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js'
+        ]
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'public/style.min.css': ['public/style.css']
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'public/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    shell: {
+      prodServer: {
+        command: 'nodemon <%= nodemon.dev %>'
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    'pkg',
+    'concat',
+    'nodemon',
+    'uglify',
+    'jshint',
+    'cssmin',
+    'watch',
+    'shell'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')) {
+      // add your production server task here
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    // add your deploy tasks here
+  ]);
+
+
+};
